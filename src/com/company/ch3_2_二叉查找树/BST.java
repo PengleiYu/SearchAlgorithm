21bca34f09adba47c821f59a303cd06c72c2f2e6
penglei <yupengniaosanshi@gmail.com>
2019-10-05 12:48:23 +0800

    3.1.2 二叉查找树：补全API

--- src/com/company/ch3_2_二叉查找树/BST.java
+++ src/com/company/ch3_2_二叉查找树/BST.java
@@ -2,7 +2,15 @@ package com.company.ch3_2_二叉查找树;
 
 import com.company.ch3_1_符号表.ST;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
+    public static void main(String[] args) {
+        ST<String, Integer> st = new BST<>();
+        ST.stTestCase(st);
+    }
+
     private Node root;
 
     @Override
@@ -30,7 +38,7 @@ public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
 
     @Override
     public void put(Key key, Value val) {
-        put(root, key, val);
+        root = put(root, key, val);
     }
 
     private Node put(Node x, Key key, Value val) {
@@ -44,55 +52,163 @@ public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
         return x;
     }
 
-
-    @Override
-    public void delete(Key key) {
-
-    }
-
-    @Override
-    public boolean contains(Key key) {
-        return false;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
     @Override
     public Key min() {
-        return null;
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        return min(x.left);
     }
 
     @Override
     public Key max() {
-        return null;
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) return x;
+        return max(x.right);
     }
 
     @Override
     public Key floor(Key key) {
-        return null;
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
     }
 
     @Override
     public Key ceiling(Key key) {
-        return null;
+        Node x = ceiling(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node ceiling(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+
+        if (cmp == 0) return x;
+        if (cmp > 0) return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t != null) return t;
+        else return x;
     }
 
     @Override
     public int rank(Key key) {
-        return 0;
+        return rank(root, key);
+    }
+
+    private int rank(Node x, Key key) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(x.left, key);
+        if (cmp > 0) return size(x.left) + 1 + rank(x.right, key);
+        return size(x.left);
     }
 
     @Override
     public Key select(int k) {
-        return null;
+        Node x = select(root, k);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+        int size = size(x.left);
+        if (k < size) return select(x.left, k);
+        if (k > size) return select(x.right, k - size - 1);
+        return x;
+    }
+
+    @Override
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x == null) return null;
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    @Override
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x == null) return null;
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    @Override
+    public void delete(Key key) {
+        delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.left == null) return x.right;
+            if (x.right == null) return x.left;
+            Node old = x;
+            x = max(x.left);
+            x.left = deleteMax(old.left);
+            x.right = old.right;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
     }
 
     @Override
     public Iterable<Key> keys(Key lo, Key hi) {
-        return null;
+        Queue<Key> queue = new LinkedList<>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmpLo = lo.compareTo(x.key);
+        int cmpHi = hi.compareTo(x.key);
+        //这三个条件很经典
+        if (cmpLo < 0) keys(x.left, queue, lo, hi);
+        if (cmpLo <= 0 && cmpHi >= 0) queue.add(x.key);
+        if (cmpHi > 0) keys(x.right, queue, lo, hi);
+    }
+
+    @Override
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
     }
 
     private class Node {
@@ -101,7 +217,7 @@ public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
         Node left, right;
         int N;
 
-        public Node(Key key, Value val, int N) {
+        Node(Key key, Value val, int N) {
             this.key = key;
             this.val = val;
             this.N = N;
